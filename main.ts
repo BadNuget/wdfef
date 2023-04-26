90cf51b236974d6d021eb152d7be035eb8008705
BadNuget <130556976+BadNuget@users.noreply.github.com>
2023-04-26 12:41:36 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,43 @@
+let Velocidad = 40
 basic.forever(function () {
-	
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 10) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) == 1 && maqueen.Ultrasonic(PingUnit.Centimeters) == 0) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Velocidad)
+            maqueen.motorStop(maqueen.Motors.M2)
+        }
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) == 1) {
+            maqueen.motorStop(maqueen.Motors.M1)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad)
+        }
+        if (true) {
+            maqueen.motorStop(maqueen.Motors.M2)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Velocidad)
+        }
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    }
+})
+basic.forever(function () {
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Half))
+    music.playTone(175, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Half))
 })
